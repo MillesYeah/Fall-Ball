4e5e7d91a285facc4e9e9b5fb6f051f84dfc8d58
Milles Creators <gabeporto13@outlook.com>
2026-09-16 22:09:27 -0300

    Fall-ball beta 5
    New updates using the bucket, and a new prototype ball

--- Assets/Spripits/Movimentação do cano.cs
+++ Assets/Spripits/Movimentação do cano.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 public class MovimentaçãoDoCano : MonoBehaviour
 {
     public float velocidade = 5f;
-    public float limiteX= 8f;
+    public float limiteX= 42f;
+    public float limiteX2 = 42f;
     void Update()
     {
         bool isPressingLeft = Input.GetKey(KeyCode.A);
@@ -12,12 +13,12 @@ public class MovimentaçãoDoCano : MonoBehaviour
         {
             return;
         }
-        if (isPressingRight)
+        if (isPressingLeft)
         {
             moviment *= -1;
         }
         transform.position += new Vector3(moviment, 0f, 0f);
-        float clampedX = Mathf.Clamp(transform.position.x, -limiteX, limiteX);
+        float clampedX = Mathf.Clamp(transform.position.x, -limiteX2, limiteX);
         transform.position = new Vector3(clampedX, transform.position.y, transform.position.z);
     }
 }
